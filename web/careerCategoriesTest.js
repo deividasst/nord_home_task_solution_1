2700c618c9a330739945fcab20a60c26ca125b56
Deividas <stancikasdeividas@gmail.com>
2022-12-05 02:43:16 +0200

    spacing

--- web/careerCategoriesTest.js
+++ web/careerCategoriesTest.js
@@ -3,11 +3,11 @@ const { url } = require('../data/apiUrlData');
 Feature('Career Categories');
 
 Scenario('should count catogries number on the page', async ({ I, careerCategoriesPage }) => {
-    I.amOnPage(url.tesonetTarget);
-    I.waitForVisible(careerCategoriesPage.menuItem.career);
-    I.click(careerCategoriesPage.menuItem.career);
-    I.seeInCurrentUrl(url.tesonetCareer);
-    I.waitForVisible(careerCategoriesPage.boxItem.jobCard);
-    const numOfElements = await I.grabNumberOfVisibleElements(careerCategoriesPage.boxItem.jobCard);
-    I.say('Number of visible career category elements is: ' + numOfElements);
+  I.amOnPage(url.tesonetTarget);
+  I.waitForVisible(careerCategoriesPage.menuItem.career);
+  I.click(careerCategoriesPage.menuItem.career);
+  I.seeInCurrentUrl(url.tesonetCareer);
+  I.waitForVisible(careerCategoriesPage.boxItem.jobCard);
+  const numOfElements = await I.grabNumberOfVisibleElements(careerCategoriesPage.boxItem.jobCard);
+  I.say('Number of visible career category elements is: ' + numOfElements);
 }).tag('@web').tag('@career');
